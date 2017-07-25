478e7ce0d54f19a9664360514ed9a1b42f9e2149
Zach Schwartz <zischwartz@gmail.com>
2017-07-25 10:11:52 -0400

    made info thing work, moved to top of window

--- readme.md
+++ readme.md
@@ -2,10 +2,12 @@
 This is the source code for [deathmapnyc.com](http://deathmapnyc.com).
 
 
-### Limitations
+### Limitations & Caveats
+- The data is incomplete, there are many ways people die that aren't captured by any of the four data sources that were available.
 - Some records contain multiple deaths, especially the OSHA and motor vehicle records. These still appear as one icon on the map.
-- The data is messy, some deaths aren't displayed because there wasn't enough information
-- The location data is particularly messy, and is often the result of geocoding an address, which may or may not be accurate, and for OSHA data may in some cases be the address of the employer rather than the location of the accident
+- The data is messy, some deaths aren't displayed because there isn't enough information.
+- The location data is particularly messy, and is often the result of geocoding an address in the jupyter notebook using google maps, which may or may not be accurate.
+- OSHA records may in some cases be the address of the employer rather than the location of the accident.
 
 
 ### 2015 Version
--- src/components/app.js
+++ src/components/app.js
@@ -8,7 +8,7 @@ import Map from './map.js'
 
 let skull_url = require("../../img/skull_d2.png")
 // let skull_url = require("../../img/skull_d2.png")
-require("file-loader?name=image.jpg!../../img/image.jpg");
+require("file-loader?name=image.jpg!../../img/deathmapnyc.jpg");
 require("file-loader?name=favicon.ico!../../img/favicon.ico");
 // => picture.png
 
@@ -21,12 +21,19 @@ class App extends React.Component {
 
   }
   render(){
-    let info_style = {display:"block"} //this.state.show_moreinfo ? {display:"block"} : {display:"none"}
+    let p_style = this.state.show_moreinfo ? {display:"block"} : {display:"none"}
+    let label = this.state.show_moreinfo ? 'Less': 'More info'
+    // style={p_style}
     return <div id="app">
-      <div id="info" style={info_style}>
+      <div id="info">
         <img src={skull_url} style={ {'float':'left', marginTop:-5, width: 20, height:27} }/>
-        <div className='title'><strong>Death in New York City</strong></div>
-        <p><a className='moreinfo' href="#" onClick={()=> this.setState({show_moreinfo:false})}>More info</a></p>
+        <div className='title'>
+          <strong>Death in New York City</strong>
+          <a className='moreinfo' href="#" onClick={()=> this.setState({show_moreinfo:!this.state.show_moreinfo})}>{label}</a>
+        </div>
+        <p style={p_style}>This is a map of deaths that occurred in New York City in 2015 and 2016. It's incomplete and imperfect.</p>
+        <p style={p_style}>For some categories/data-sources there's data going as far back as 2006. Check the "Show All Years" button to see that.</p>
+        <p style={p_style}>For more information, see limitations or the github repo.</p>
       </div>
       <Map/>
     </div>
